877c0071b24152441a47a9f08703b160c0265bc0
wpunk <wpunk@yandex.ru>
2015-09-27 22:17:24 +0300

    lesson9

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -22,7 +22,9 @@ namespace ConsoleApplication1
             rightline.Draw();
 
             Point p = new Point(4, 5, '*');
-            p.Draw();
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
+
 
             Console.ReadLine();
         }
