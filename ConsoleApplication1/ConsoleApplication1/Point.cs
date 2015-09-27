95ea1dde1d80d77a7e1dc174146fabc4d6d4112b
wpunk <wpunk@yandex.ru>
2015-09-27 20:38:38 +0300

    lesson4

--- ConsoleApplication1/ConsoleApplication1/Point.cs
+++ ConsoleApplication1/ConsoleApplication1/Point.cs
@@ -12,6 +12,18 @@ namespace ConsoleApplication1
         public int y;
         public char sym;
 
+        public Point()
+        {
+
+        }
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
